4060ecdc854816ebba52ea67d3fcdead09c4240e
Joseph Michaels <jmichaels@gmail.com>
2014-06-27 00:03:37 +0900

    Initial Commit

---
+++ MLSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MLSwift
+//
+//  Created by Joseph Michaels on 6/27/14.
+//  Copyright (c) 2014 Joseph Michaels. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MLSwift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MLSwift
+//
+//  Created by Joseph Michaels on 6/27/14.
+//  Copyright (c) 2014 Joseph Michaels. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)

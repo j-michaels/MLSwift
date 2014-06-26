8b219eb86fa7532a4da33e27e13a19a293a3dd1e
Joseph Michaels <jmichaels@gmail.com>
2014-06-27 00:17:11 +0900

    Added HMM class stubs

---
+++ MLSwift/hmm.swift
@@ -0,0 +1,35 @@
+//
+//  hmm.swift
+//  MLSwift
+//
+//  Created by Joseph Michaels on 6/27/14.
+//  Copyright (c) 2014 Joseph Michaels. All rights reserved.
+//
+
+import Foundation
+
+operator infix ** {}
+
+func ** (num: Double, power: Double) -> Double {
+    return pow(num, power)
+}
+
+class State {
+    init() {
+        
+    }
+}
+
+class HMM {
+    
+}
+
+class SentenceHMM: HMM {
+    init() {
+        
+    }
+}
+
+class ModelManager {
+    
+}
